07aacbc6819498491caf629b5ab4976b4661d437
CharleeJennings <Charlesjenn39@gmail.com>
2018-11-26 12:48:23 -0600

    Added splashpage back for production

--- src/client/components/Splashpage.js
+++ src/client/components/Splashpage.js
@@ -30,7 +30,7 @@ basicTimeline.add
 .add({
 
  	targets: '#lineDrawing svg',
- 	top: 0,
+ 	top: -65,
  	left: 0 ,
  	scale: .25,
  	delay : 0,
@@ -67,7 +67,7 @@ return ( <div>
 
 	<div id ="background"/>
 	<div id = 'lineDrawing' >
-<svg style ={{left: '45%'}} width="155" height="191" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF"  class ="lines" strokeWidth="3" fill="none" fillRule="evenodd" stroke-linecap="square"><path d="M2 44v118.147l65.857 26.167v-30.85l-41.721-16.517V96.173l41.721 14.393V68.265z"/><path d="M68.665 188.073l9.46-4.113 5.648-2.456v-27.925L40 137.829v-36.467M68.302 110.273l15.754-6.977V63.224L23.2 40 2 43.503M27.5 140.5l13-2"/><path d="M67.703 157.84l47.498-13.593V36.842l37.25 3.495V15.962L46.87 2.679v26.54l37.572 4.871v30.894M38.5 37.5l8-8"/><path d="M67.472 55.636V43.808L36.317 38.85 37.7 7.16l7.565-4.265M115.814 48.578l30.647 2.748 6.006-9.852M67.5 44.5l15-9M68.5 68.5l14-5"/></g></svg>	 </div>
+<svg style ={{left: '45%', top: "300px"}} width="155" height="191" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF"  class ="lines" strokeWidth="3" fill="none" fillRule="evenodd" stroke-linecap="square"><path d="M2 44v118.147l65.857 26.167v-30.85l-41.721-16.517V96.173l41.721 14.393V68.265z"/><path d="M68.665 188.073l9.46-4.113 5.648-2.456v-27.925L40 137.829v-36.467M68.302 110.273l15.754-6.977V63.224L23.2 40 2 43.503M27.5 140.5l13-2"/><path d="M67.703 157.84l47.498-13.593V36.842l37.25 3.495V15.962L46.87 2.679v26.54l37.572 4.871v30.894M38.5 37.5l8-8"/><path d="M67.472 55.636V43.808L36.317 38.85 37.7 7.16l7.565-4.265M115.814 48.578l30.647 2.748 6.006-9.852M67.5 44.5l15-9M68.5 68.5l14-5"/></g></svg>	 </div>
 
 
 	</div>);
